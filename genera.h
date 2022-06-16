e3a073340a8bb2a8b196d95a6c0f053019bd7451
Giominoccari <98152727+Giominoccari@users.noreply.github.com>
2022-06-16 10:39:07 +0200

    Create genera.h

---
+++ genera.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+#include "struct.h"
+
+void inserisci_scala(casella cas);
+void inserisci_scivolo(casella cas);
+void salta_turno(casella cas);
+
+#endif
