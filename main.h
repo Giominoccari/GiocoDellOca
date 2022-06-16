a59667591f72a5c12902ffbdd35f6fe4e42fbd09
Giominoccari <98152727+Giominoccari@users.noreply.github.com>
2022-06-16 10:38:25 +0200

    Create main.h

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H_INCLUDED
+#define FUNCTIONS_H_INCLUDED
+
+#include "struct.h"
+
+void genera_partita(casella *tab);
+void gioca_partita(giocatore *t, giocatore *c, casella *tab);
+
+#endif
